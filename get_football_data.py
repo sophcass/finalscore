8632443f3611fa5ef19d0fd05def6dae52a62020
Sophie Cassidy <sophie.cassidy@ly.st>
2025-01-29 08:44:17 +0000

    Get League ID to be used in API calls to get match statistics

--- get_football_data.py
+++ get_football_data.py
@@ -1,15 +1,29 @@
 import http.client
+import json
+import os
 
 conn = http.client.HTTPSConnection("api-football-v1.p.rapidapi.com")
 
+key = os.getenv("RAPIDAPI_KEY")
+
 headers = {
-    "x-rapidapi-key": "XXXXXxxxxx",
+    "x-rapidapi-key": key,
     "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
 }
 
-conn.request("GET", "/v3/leagues", headers=headers)
+conn.request("GET", "/v3/leagues?name=Premier+League&code=GB-ENG", headers=headers)
 
 res = conn.getresponse()
 data = res.read()
 
-print(data.decode("utf-8"))
+data = json.loads(data.decode("utf-8"))
+
+leagues = data["response"]
+
+if len(leagues) > 1:
+    raise Exception("More than one league in response")
+
+league = leagues[0]
+league_id = league["league"]["id"]  # 39 - to be used in other API calls
+
+print(league_id)
